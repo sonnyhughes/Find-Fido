4991788246161df2e955fc214a3c84f99bc418f4
Scott Saltzman <salty923@gmail.com>
2018-01-20 20:22:00 -0600

    merge login js into main.  Remove the build map button and add map on submit button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,9 +3,80 @@ $(document).ready(function(){
     // Firebase
     var dogData = new Firebase("https://nwuproject1.firebaseio.com/");
 
+    //maps
+    var geocoder;
+    var map;
+    var markers = [];
+
 
     // Button to add Dogs
     $("#addDog").on("click", function(){
+
+        //show map
+        $(".showMap").addClass("map");
+
+        //map logic
+        var address = $('#addr').val();
+
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                //console.log('geocoder results:');
+                console.dir(results);
+
+                var mapOptions = {
+                    zoom: 16,
+                    mapTypeControl: true,
+                    mapTypeControlOptions: {
+                        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+                    },
+                    zoomControl: true,
+                    zoomControlOptions: {
+                        style: google.maps.ZoomControlStyle.SMALL
+                    },
+                    //streetViewControl: false,
+                    center: results[0].geometry.location
+                }
+
+                map = new google.maps.Map(document.getElementById('map1'), mapOptions);
+
+
+                //map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    title: "Lost Dog",
+                    label: "Lost",
+                    position: results[0].geometry.location,
+                    draggable: false,
+                    animation: google.maps.Animation.DROP
+                });
+
+                var contentString = '<div id="content">' +
+                    '<div id="siteNotice">' +
+                    '</div>' +
+                    '<h1 id="firstHeading" class="firstHeading">Lost Dog</h1>' +
+                    '<div id="bodyContent">' +
+                    '<p><b>Lost Dog</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                    'sandstone rock formation in the southern part of the ' +
+                    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+                    'south west of the nearest large town, Alice Springs;</p>' +
+                    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+                    'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+                    '(last visited June 22, 2009).</p>' +
+                    '</div>' +
+                    '</div>';
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
+
+            } else {
+                alert('Please try a different address.');
+            }
+        })
 
 
         // Parses input values and attaches them to a variable      
--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,5 +1,20 @@
-markers.push(marker);
 
-for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map2'), {
+        center: { lat: 41.87, lng: -87.63 },
+        zoom: 8
+    });
 }
+
+
+
+
+
+
+// markers.push(marker);
+
+// for (var i = 0; i < markers.length; i++) {
+//     markers[i].setMap(map);
+// }
